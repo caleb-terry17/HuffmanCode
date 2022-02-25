3ac016b1e3c252e08efdba4a70a42c557bafe9ba
caleb-terry17 <k.cterry30@gmail.com>
2022-02-25 00:01:26 -0600

    designed huffman algo

--- logic.js
+++ logic.js
@@ -1,12 +1,94 @@
 ///////////////////
+// imports
+///////////////////
+import fs from 'fs';  // for reading from files 
+
+///////////////////
 // globals
 ///////////////////
 // document tags to display compare values
 let ss = document.getElementById("hcOut");
 
 ///////////////////
+// classes
+///////////////////
+// NOTE: the leafs will all be "Char" obj's while rest of the nodes in the tree will be "Node's"
+
+// node object for the binary tree which keeps track of a letters frequency
+class Node {
+    constructor(l, r) {
+        // left and right child of the current node
+        // NOTE: r.freq >= l.freq
+        this.left = l;  // 0
+        this.right = r;  // 1
+        // frequency of left and right childs
+        this.freq = l.freq + r.freq;
+    }
+}
+
+// holds a character and it's frequency
+class Char {
+    constructor(c, f) {
+        // character represented 
+        this.char = c;
+        // chars frequency 
+        this.freq = f;
+    }
+}
+
+///////////////////
+// helper functions
+///////////////////
+// inserts an element of an array in sorted order
+function pushSort(ary, elem) {
+    ary.push(elem);  // element is now last element in ary
+    let idx = ary.length - 1;
+    let temp;
+    while (idx > 0 && ary[idx].freq < ary[idx - 1].freq) {
+        // swap
+        temp = ary[idx];
+        ary[idx] = ary[idx - 1];
+        ary[idx - 1] = temp;
+    }
+    return ary;
+}
+
+///////////////////
 // huffman code calculation
 ///////////////////
+// takes a list of Char's and constructs a tree
+function makeTree(list) {
+    let l, r;  // left and right child to be popped from list
+    // want final length of list to be 1 
+    while (list.length > 1) {
+        // l <= r
+        l = list.shift();
+        r = list.shift();
+        // add new node
+        pushSort(list, new Node(l, r));
+    }
+    return list[0];  // parent node of tree
+}
+
+// given a file, reads in and counts the frequencies of each character
+function countFreq(fileName) {
+    let freqList;
+    fs.readFile(fileName, 'utf-8', (err, file) => {
+        if (err) { throw err; }  // check for error
+        file = file.toString().sort();
+        // check for 0 length file
+        if (file.length == 0) { return []; }
+        // read file data and count frequencies
+        freqList = [new Char(file[0], 1)];
+        let idx = 0;
+        for (let i = 1; i < file.length; ++i) {
+            // see if character has already been added
+            if (file[i] == file[i - 1]) { freqList[idx]++; }
+            else { freqList[++idx] = new Char(file[i], 1); }
+        }
+    });
+    return freqList;
+}
 
 ///////////////////
 // i/o
