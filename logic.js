ce1dd6570ec9398fddcc8ac8ffb5ba466d9e6ce0
caleb-terry17 <k.cterry30@gmail.com>
2022-02-26 14:28:56 -0600

    started implementation of huffman code from user input

--- logic.js
+++ logic.js
@@ -7,7 +7,7 @@ import fs from 'fs';  // for reading from files
 // globals
 ///////////////////
 // document tags to display compare values
-let ss = document.getElementById("hcOut");
+let hc = document.getElementById("hcOut");
 
 ///////////////////
 // classes
@@ -104,51 +104,21 @@ function listToString(list) {
     return str;
 }
 
-// called by html button, computes and outputs lcs
-function computeSS() {
+// called by html button, computes and outputs huffman encoding
+function computeHC() {
     // get input from list1, list2
-    let set = document.getElementById("set").value;
-    let k = document.getElementById("sum").value;
+    let chars = document.getElementById("chars").value;
 
     // checking for too large of input
-    if (set.length > 200) {
-        ssOut.innerHTML = "<h3>Input Too Large (>100 elements)</h3>";
+    if (chars.length > 1000) {
+        ssOut.innerHTML = "<h3>Input Too Large (>1000 characters)</h3>";
         return; 
-    } else if (k.length > 3) {
-        ssOut.innerHTML = "<h3>Input Too Large (>999)</h3>";
-        return; 
-    }
-
-    // remove all whitespace
-    set = set.split(" ").join("");
-
-    // separate by ,
-    set = set.split(",");
-
-    // converting to ints
-    list = [];
-    k = parseInt(k);
-    for (let i = 0; i < set.length; ++i) {
-        list[i] = parseInt(set[i]);
     }
 
     // sorting set
-    list.sort();
-
-    // making sure all positive numbers
-    if (list[0] < 1) {
-        ssOut.innerHTML = "<h3>Must be all Positive Numbers</h3>";
-    }
-
-    // making sure elements are not repeating
-    for (let i = 1; i < list.length; ++i) {
-        if (list[i] == list[i - 1]) {
-            ssOut.innerHTML = `<h3>Not a Set (repeating elements: ${list[i]})</h3>`;
-            return;
-        }
-    }
+    chars = chars.sort();
 
     // constructing output
-    out = subsetSum(list, k);
-    ssOut.innerHTML = `<h3>It ${out.ss ? `is` : `is not`} possible to construct a sum of ${k} from the set ${listToString(list)}`;
+    out = chars;
+    hc.innerHTML = `<h3>The corresponding Huffman Coding is: ${out}</h3>`;
 }
